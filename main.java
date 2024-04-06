501c50db05c1288657b40b5bb15491d282ab44dc
Robert Matuszewski <robert.matuszewski556@gmail.com>
2024-04-06 09:47:43 +0200

    h

--- main.java
+++ main.java
@@ -1,11 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        // Tworzenie obiektu klasy Point i inicjowanie jego wartości
-        point point = new point(3.5, 2.0);
+        point punkt= new point(5.0, 3.0);
 
-        // Wyświetlanie współrzędnych punktu
-        System.out.println("Współrzędne punktu:");
-        System.out.println("x = " + point.getX());
-        System.out.println("y = " + point.getY());
+        System.out.println("współrzędne to:");
+        System.out.println("x- " + punkt.getX());
+        System.out.println("y- " + punkt.getY());
     }
 }
